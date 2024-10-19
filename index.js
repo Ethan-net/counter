bb2f114cc2131ea2804f6d0457909e35146c6d3f
Ethan-net <koredeajibola@gmail.com>
2024-10-19 21:08:32 +0100

    updated

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const countDisplay = document.querySelector(".countDisplay");
+const previousCount = document.getElementsByClassName("precount");
 
+
+console.log(previousCount)
 let count = 0;
 
 function countClick() {
     count++
     // console.log(count);
-    countDisplay.textContent = count;
+    countDisplay.innerText = count;
+}
+
+function save() {
+    let countStr = count + "-";
+    // previousCount.innerText += countStr;
+    // console.log(countStr)
+    previousCount.innertext = countStr;
+    countDisplay.textContent = 0;
+    count = 0
 }
